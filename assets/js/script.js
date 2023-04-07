7025ae4f6ed7e6334bf1e45d110f2aac51a6d2b9
afrazier01 <amfrazier01@gmail.com>
2023-04-06 22:22:04 -0500

    Added text to buttons for quiz and working on correct vs incorrect actions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,248 @@
-var submitBtn = document.querySelector('.start-timer');
+var startBtn = document.querySelector('.start-timer');
 var quizTimer = document.querySelector('#time')
-submitBtn.textContent = 'Start Timer';
+var seeScore = document.querySelector('#score')
+startBtn.textContent = 'Start Timer';
 
-var timeLeft = 15;
+
+var userScore = 0
+var timeLeft = 60;
 //set to 60 for actual quiz
 
+
+//Timer
 function quizTime () {
+    console.log('Button was pressed. Timer has started')
+    var displayQuizContent = document.querySelector('.quizPopUp')
+    seeScore.textContent = userScore
+    displayQuizContent.setAttribute('style','display:flex')
+    quizAssessment()
+
+    if (timeLeft <= 0) {
+        return
+        //set parameter so it checks time before starting timer+
+    }
+
     var timerInterval = setInterval(function() {
-        console.log('Button was pressed. Timer has started')
         timeLeft--;
         quizTimer.textContent = 'Time left:' + timeLeft
 
         if (timeLeft <= 10) {
-            quizTimer.setAttribute('style', 'color: red; font-weight: bolder;')
+            console.log('User has less than 10 seconds!')
         }
 
-        if (timeLeft === 0) {
+        
+
+        if (timeLeft <= 10 && timeLeft % 2 == 0) {
+            quizTimer.setAttribute('style', 'color: red; font-weight: bolder;')
+        } else {
+            quizTimer.setAttribute('style','color: #fff;')
+        }
+
+        if (timeLeft <= 0) {
             clearInterval(timerInterval);
-            quizAssessment()
+            console.log('Quiz timer has ended')
         }
         
     }, 1000)
 }
 
+startBtn.addEventListener('click', quizTime)
+
 function quizAssessment () {
-    console.log('Quiz timer has ended')
-    
+    var displayQuizContent = document.querySelector('.quizPopUp')
+    displayQuizContent.setAttribute('style','display:flex')
+
+    // add quiz content
+}
+
+function scoresList () {
+    // create var that selects the proper div
+    //set the attribute to display it
 }
 
 
-submitBtn.addEventListener('click', quizTime)
+//Quiz Content
+var quizPrompts = ['What is your favorite color?','What is your dream job?']
+var option1 = ['red','race car driver']
+var option2 = ['blue','professional athlete']
+var option3 = ['black','salesman']
+var option4 = ['green','pilot']
+
+var quizText = document.querySelector('#questions')
+var button1 = document.querySelector('#b1')
+var button2 = document.querySelector('#b2')
+var button3 = document.querySelector('#b3')
+var button4 = document.querySelector('#b4')
+
+indexCount = 0
+
+//Sets text for question and buttons
+quizText.textContent = quizPrompts[indexCount]
+button1.textContent = option1[indexCount]
+button2.textContent = option2[indexCount]
+button3.textContent = option3[indexCount]
+button4.textContent = option4[indexCount]
+
+button1.addEventListener('click', function(){
+    console.log('The first button was pressed');
+    indexCount += 1
+
+    if (indexCount >= quizPrompts.length) {
+        indexCount = indexCount - quizPrompts.length
+    }
+    if (indexCount >= option1.length) {
+        indexCount = indexCount - option1.length
+    }
+    if (indexCount >= option2.length) {
+        indexCount = indexCount - option2.length
+    }
+    if (indexCount >= option3.length) {
+        indexCount = indexCount - option3.length
+    }
+    if (indexCount >= option4.length) {
+        indexCount = indexCount - option4.length
+    }
+
+    // if statement that looks for which question
+
+    quizText.textContent = quizPrompts[indexCount]
+    button1.textContent = option1[indexCount]
+    button2.textContent = option2[indexCount]
+    button3.textContent = option3[indexCount]
+    button4.textContent = option4[indexCount]
+})
+
+button2.addEventListener('click', function(){
+    console.log('The second button was pressed');
+    indexCount += 1
+
+    if (indexCount >= quizPrompts.length) {
+        indexCount = indexCount - quizPrompts.length
+    }
+    if (indexCount >= option1.length) {
+        indexCount = indexCount - option1.length
+    }
+    if (indexCount >= option2.length) {
+        indexCount = indexCount - option2.length
+    }
+    if (indexCount >= option3.length) {
+        indexCount = indexCount - option3.length
+    }
+    if (indexCount >= option4.length) {
+        indexCount = indexCount - option4.length
+    }
+
+    // if statement that looks for which question
+
+    quizText.textContent = quizPrompts[indexCount]
+    button1.textContent = option1[indexCount]
+    button2.textContent = option2[indexCount]
+    button3.textContent = option3[indexCount]
+    button4.textContent = option4[indexCount]
+})   
+
+button3.addEventListener('click', function(){
+    console.log('The third button was pressed');
+    indexCount += 1
+
+    if (indexCount >= quizPrompts.length) {
+        indexCount = indexCount - quizPrompts.length
+    }
+    if (indexCount >= option1.length) {
+        indexCount = indexCount - option1.length
+    }
+    if (indexCount >= option2.length) {
+        indexCount = indexCount - option2.length
+    }
+    if (indexCount >= option3.length) {
+        indexCount = indexCount - option3.length
+    }
+    if (indexCount >= option4.length) {
+        indexCount = indexCount - option4.length
+    }
+
+    // if statement that looks for which question
+
+    quizText.textContent = quizPrompts[indexCount]
+    button1.textContent = option1[indexCount]
+    button2.textContent = option2[indexCount]
+    button3.textContent = option3[indexCount]
+    button4.textContent = option4[indexCount]
+})
+
+button4.addEventListener('click', function(){
+    console.log('--');
+    console.log('The fourth button was pressed');
+    indexCount += 1
+    
+    // if statement that looks for which question
+    
+
+    //default action should be user incorrect so that correct would have a special condition
+    
+
+    //if you want the correct answer to be on this button add it here
+    if (indexCount === 1) {
+        console.log('User selected the RIGHT option')
+        userScore += 100
+        console.log(userScore)
+    } else {console.log('User selected the WRONG option')}
+
+
+
+
+    if (indexCount >= quizPrompts.length) {
+        indexCount = indexCount - quizPrompts.length
+    }
+    if (indexCount >= option1.length) {
+        indexCount = indexCount - option1.length
+    }
+    if (indexCount >= option2.length) {
+        indexCount = indexCount - option2.length
+    }
+    if (indexCount >= option3.length) {
+        indexCount = indexCount - option3.length
+    }
+    if (indexCount >= option4.length) {
+        indexCount = indexCount - option4.length
+    }
+
+    
+
+    quizText.textContent = quizPrompts[indexCount]
+    button1.textContent = option1[indexCount]
+    button2.textContent = option2[indexCount]
+    button3.textContent = option3[indexCount]
+    button4.textContent = option4[indexCount]
+    seeScore.textContent = userScore
+})
+
+//renderlastscorefunction
+
+
+//var firstNameInput = document.querySelector("#first-name");
+// var lastNameInput = document.querySelector("#last-name");
+// var emailInput = document.querySelector("#email");
+// var passwordInput = document.querySelector("#password");
+// var signUpButton = document.querySelector("#sign-up");
+
+// signUpButton.addEventListener("click", function(event) {
+//   event.preventDefault();
+  
+//   // TODO: Create user object from submissio
+//   var signUp = {
+//     firstName: firstNameInput.value,
+//     lastName: lastNameInput.value,
+//     email: emailInput.value,
+//     password: passwordInput.value
+//   }
+
+//   localStorage.setItem('signUp', JSON.stringify(signUp));
+//   // TODO: Set new submission to local storage 
+// function renderMessage() {
+//     var lastGrade = JSON.parse(localStorage.getItem("studentGrade"));
+//     if (lastGrade !== null) {
+//       document.querySelector(".message").textContent = lastGrade.student + 
+//       " received a/an " + lastGrade.grade
+// });
+
